78cb808834369264d5147566886b91e57d89b79f
Abdullah Çok <abdullahcok@yandex.com>
2022-07-12 00:41:47 +0300

    some projects updated

--- src/Data.js
+++ src/Data.js
@@ -1,32 +1,32 @@
 export const portfolio = [
   {
-    id: 1,
-    img: 'https://blog.abdullahcok.dev/wp-content/uploads/2022/04/sirius.png',
-    link: "http://siriusytg.com",
+      id: 1,
+      img: "http://blog.abdullahcok.dev/wp-content/uploads/2022/04/dht.png",
+      link: "http://drhamditurhan.com",
   },
   {
-    id: 2,
-    img: 'https://blog.abdullahcok.dev/wp-content/uploads/2022/04/ktaksi.png',
-    link: "http://kutahyadataksi.com",
+      id: 2,
+      img: "http://blog.abdullahcok.dev/wp-content/uploads/2022/07/mefsan.png",
+      link: "http://mefsan.com.tr",
   },
   {
     id: 3,
-    img: "https://blog.abdullahcok.dev/wp-content/uploads/2022/04/scustom.png",
-    link: "/projects/scustom/index.html",
-  },
+    img: "http://blog.abdullahcok.dev/wp-content/uploads/2022/04/sirius.png",
+    link: "http://siriusytg.com",
+ },
   {
     id: 4,
-    img: "https://blog.abdullahcok.dev/wp-content/uploads/2022/04/abt.png",
-    link: "/projects/abt/index.html",
+    img: "http://blog.abdullahcok.dev/wp-content/uploads/2022/04/sinsaat.png",
+    link: "http://sumbullerinsaat.com",
   },
   {
     id: 5,
-    img: "https://blog.abdullahcok.dev/wp-content/uploads/2022/04/sinsaat.png",
-    link: "/projects/sinsaat/index.html",
+    img: 'http://blog.abdullahcok.dev/wp-content/uploads/2022/04/ktaksi.png',
+    link: "http://kutahyadataksi.com",
   },
   {
     id: 6,
-    img: "https://blog.abdullahcok.dev/wp-content/uploads/2022/04/pmuhendislik.png",
-    link: "/projects/pmuhendislik/index.html",
+    img: "http://blog.abdullahcok.dev/wp-content/uploads/2022/04/scustom.png",
+    link: "/projects/scustom/index.html",
   },
 ];
